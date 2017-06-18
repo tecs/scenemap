05354e0e3e634346d0c57293802b788063a51b94
Alexander Ivanov <ttecss@gmail.com>
2017-06-18 19:56:04 +0300

    Fixed the side panel size

--- scenemap.gd
+++ scenemap.gd
@@ -3,8 +3,8 @@ extends EditorPlugin
 
 const SCRIPT_NAME = "_Scene_Map_Node_Script_"
 
-var control = preload("control.tscn").instance()
-var menu = Control.new()
+var control = VBoxContainer.new()
+var menu = HBoxContainer.new()
 var active
 
 # Construct / Destruct
@@ -12,6 +12,14 @@ func _enter_tree():
 	var script = preload("scenemap_node.gd")
 	script.set_name(SCRIPT_NAME)
 	add_custom_type("SceneMap", "Node2D", script, preload("icon.png"))
+	var items = ItemList.new()
+	items.set_v_size_flags(ItemList.SIZE_EXPAND_FILL)
+	items.set_custom_minimum_size(Vector2(80, 0))
+	items.set_max_columns(0)
+	items.set_icon_mode(ItemList.ICON_MODE_TOP)
+	items.set_max_text_lines(2)
+	items.set_name("Items")
+	control.add_child(items)
 	add_control_to_container(CONTAINER_CANVAS_EDITOR_MENU, menu)
 	add_control_to_container(CONTAINER_CANVAS_EDITOR_SIDE, control)
 	menu.hide()
@@ -28,7 +36,7 @@ func edit(object):
 	active.selected = true
 	active.update()
 	
-	var tileContainer = control.get_node("Panel/ItemList")
+	var tileContainer = control.get_node("Items")
 	tileContainer.clear()
 	if active.sceneSet:
 		for tile in active.sceneSet.instance().get_children():
@@ -40,7 +48,7 @@ func handles(object):
 func forward_input_event(event):
 	if event.type == InputEvent.MOUSE_BUTTON and event.is_pressed():
 		if event.button_index == BUTTON_LEFT:
-			var tilesContainer = control.get_node("Panel/ItemList")
+			var tilesContainer = control.get_node("Items")
 			var selected = tilesContainer.get_selected_items()
 			if selected.size(): active.setTile(tilesContainer.get_item_text(selected[0]))
 			return true
